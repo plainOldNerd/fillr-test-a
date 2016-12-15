d808083a4e2d3ef17c4a55f3e6ae4e53f2dfdb61
Andrew Mouat <plz_nosparem_thnx@hotmail.com>
2016-12-15 18:55:46 +1100

    got some basic shit figured out for task #2

--- extract.js
+++ extract.js
@@ -1,5 +1,14 @@
 module.exports.extract = function(window) {
   
   // Write your solution to Task #2 - Extract Metadata task here
+	var document = window.document;
+	var tds = document.getElementsByTagName("td");
 
+	/* The 0th td is the horizontal 'resources' table/table data.
+	   After that, every odd numbered td will be a control label, while every 
+	   even numbered td will be the corresponding input.
+	 */
+	 for(var i = 1; i < tds.length; ++i){
+console.log(tds[i].innerHTML);
+	 }
 }
